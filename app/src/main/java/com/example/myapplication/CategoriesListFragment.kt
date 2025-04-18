7b5fa3226aa7a3e2240965db9634aaaab6e2f521
SuhachevN <tradenoff@gmail.com>
2025-04-19 00:59:26 +0500

    RA-08 realized recyclerView with categories list by adapter

--- app/src/main/java/com/example/myapplication/CategoriesListFragment.kt
+++ app/src/main/java/com/example/myapplication/CategoriesListFragment.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.myapplication.databinding.FragmentListCategoriesBinding
 
-class CategoriesListFragment : Fragment() {
+class CategoriesListFragment : Fragment(R.layout.fragment_list_categories) {
 
     private var _binding: FragmentListCategoriesBinding? = null
     private val binding: FragmentListCategoriesBinding
@@ -19,10 +19,23 @@ class CategoriesListFragment : Fragment() {
     ): View {
         _binding = FragmentListCategoriesBinding.inflate(inflater, container, false)
         return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initRecycler()
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
+    private fun initRecycler() {
+        val categories: List<Category> = STUB.getCategories()
+        val adapter = CategoriesListAdapter(categories)
+        binding.recyclerViewCategories.adapter = adapter
+    }
+
 }
